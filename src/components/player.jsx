6d18652126521dcdd232371dfa3cd9b53de42c7c
RemyJS <a.king92@mail.ru>
2020-08-23 14:23:12 +0300

    feat: add player style

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import unknowBird from '../img/unknowBird.jpg';
+import React from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import unknowBird from "../img/unknowBird.jpg";
+import frExample from '../audio/fr.mp3';
 
 export default function player() {
   return (
-    <div className='player'>
-      <div className='player__img'>
-        <img src={unknowBird} alt="unknow-bird"/>
+    <div className="player">
+      <div className="player__img">
+        <img src={unknowBird} alt="unknow-bird" />
       </div>
-      <div className='player__aside'>
+      <div className="player__aside">
         <div className="player__aside__name">
           <h1>*****</h1>
         </div>
-        <div>
-          <audio src=""></audio>
+        <div className="player__audio">
+          <AudioPlayer
+            autoPlay
+            src={frExample}
+            onPlay={(e) => console.log("onPlay")}
+            // other props here
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
